9a77254787a0202a7ba04c548542e16f4acf3eaf
hala-emam <hala.emam141@gmail.com>
2024-05-20 20:56:49 +0300

    update by hala newModel.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
+const port = 3001
+app.get('/', (req, res) => res.send('Hello World !'))
+app.get('/hala', (req, res) => res.send('Hello World Hala!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
