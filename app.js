df5feaaa4f2de12698d1a58b087c970eb3e3f687
Angelo Esam <angloesam61@gmail.com>
2024-05-20 21:03:48 +0300

    updated

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const app = express()
 const port = 3001
 app.get('/', (req, res) => res.send('Hello World !'))
 app.get('/hala', (req, res) => res.send('Hello World Hala!'))
+app.get('/angelo', (req, res) => res.send('Hello World angelo!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
