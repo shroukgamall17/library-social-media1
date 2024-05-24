0455e4330a3056e842e29250ec5f8dfc5cfdb774
moamen <momenkhaled1900081@gmail.com>
2024-05-24 23:33:11 +0300

    create post and comment model , create post controller

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
-const mongoose= require('mongoose');
-const userRoutes=require('./routes/userRoutes')
-const cors=require('cors');
-const dotenv=require('dotenv').config();
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const postRoute = require("./routes/postRoute");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
 const app = express();
 app.use(express.json());
-app.use(cors())
-app.use(express.urlencoded( { extended: false } ));  
-mongoose.connect(process.env.CONNECTION_DB).then(()=>console.log("connected to db 😃"))
-.catch((err)=>console.error(err));
-app.use('/users',userRoutes)
-app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`))
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+mongoose
+  .connect(process.env.CONNECTION_DB)
+  .then(() => console.log("connected to db 😃"))
+  .catch((err) => console.error(err));
+app.use("/users", userRoutes);
+app.use("/posts", postRoute);
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`)
+);
