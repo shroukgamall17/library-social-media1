2b119f646f54825dc6eb13df49e336553685c1a0
Jaklen22 <jaklenmagdy365@gmail.com>
2024-06-08 04:10:23 -0700

    add coverImag

--- models/bookModel.js
+++ models/bookModel.js
@@ -1,12 +1,11 @@
 const mongoose=require('mongoose');
 const bookSchema=new mongoose.Schema({
-    // authorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Author', required: true },
     title:{type:String,required:true,unique:true},
     description:{type:String},
     category:{type:String,required:true},
     Pdf:{type:String},
     cover:{type:String},
-    ratings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Rating' }],
+    ratings: [{ type:mongoose.Schema.Types.ObjectId, ref: 'Rating' }],
     averageRating: { type: Number, default: 0 },
     ratingCount: { type: Number, default: 0 }
 },{timestamps:true});
